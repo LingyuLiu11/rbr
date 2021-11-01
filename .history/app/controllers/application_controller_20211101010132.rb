7fb62fd7da78c77b54129e7060c476a225cec5f5
LingyuLiu11 <linalxly@gmail.com>
2021-11-01 01:19:22 -0400

    Finish static pages

--- .history/app/controllers/application_controller_20211101010132.rb
+++ .history/app/controllers/application_controller_20211101010132.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-    
+    def method_name
+        
+    end
 end
--- .history/test/controllers/static_pages_controller_test_20211101011412.rb
+++ .history/test/controllers/static_pages_controller_test_20211101011412.rb
@@ -1,11 +1,6 @@
-require 'test_helper'
+require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -15,12 +10,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end
